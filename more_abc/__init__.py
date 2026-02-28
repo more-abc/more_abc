3703c3b538baa1691b66979dae3d654733f6b35b
Evan Yang <quantbit@126.com>
2026-02-28 20:31:45 +0800

    Fixed Unknown classifier in field License to GNU-3.0

--- more_abc/__init__.py
+++ more_abc/__init__.py
@@ -137,7 +137,7 @@ __all__ = ["ABCMixin",
             # version of `more_abc` module
            "version"]
 
-__version__ = "2.1.12"
+__version__ = "2.1.13"
 __author__ = "Evan Yang <quantbit@126.com>"
 __license__ = "GPL-3.0"
 # Can be development / stable / deprecated
