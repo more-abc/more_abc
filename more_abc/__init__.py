ae8aa3ebc79dc130d3bf01929ec69703f66cac9c
Evan Yang <quantbit@126.com>
2026-03-08 18:21:49 +0800

    Simplify the README.md

--- more_abc/__init__.py
+++ more_abc/__init__.py
@@ -183,9 +183,9 @@ __all__ = ["ABCMixin",
            # Development mode toggle switch   
            "DevMode"]
 
-__version__ = "2.2.8"
-__version_tuple__ = (2, 2, 8)
-__int_version__ = 45
+__version__ = "2.2.9"
+__version_tuple__ = (2, 2, 9)
+__int_version__ = 46
 __author__ = "Evan Yang <quantbit@126.com>"
 __license__ = "GPL-3.0"
 # Can be development / stable / deprecated
